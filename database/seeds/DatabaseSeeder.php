48918c72ae49db0b5b7fcd55ed437ae2f9997bb0
unknown <muhammad.elman14@gmail.com>
2021-07-28 10:03:28 +0800

    Update Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             Userseeder::class,
             bukuseeder::class,
-            peminjamanseeder::class,
+            
         ]);
     }
 }
--- database/seeds/peminjamanseeder.php
+++ database/seeds/peminjamanseeder.php
@@ -11,28 +11,29 @@ class peminjamanseeder extends Seeder
      */
     public function run()
     {
-        DB::table('peminjamen')->insert([[
-                'tanggal_pinjam' => '21-02-01',
-                'tanggal_kembali' => '21-03-01',
-                'nama_peminjam' => 'fatmah',
-                'alamat' => 'bumi',
-                'buku_id' => 1
+//         DB::table('peminjamen')->insert([[
+//                 'tanggal_pinjam' => '21-02-01',
+//                 'tanggal_kembali' => '21-03-01',
+//                 'nama_peminjam' => 'fatmah',
+//                 'alamat' => 'bumi',
+//                 'buku_id' => 1
                       
             
-        ],
-    [
-        'tanggal_pinjam' => '20-05-01',
-        'tanggal_kembali' => '20-5-10',
-        'nama_peminjam' => 'eli',
-        'alamat' => 'mataram',
-        'buku_id' => 2
-    ],
-[
-    'tanggal_pinjam' => '21-07-01',
-    'tanggal_kembali' => '21-08-05',
-    'nama_peminjam' => 'fina',
-    'alamat' => 'waduwani',
-    'buku_id' => 1
-]]);
+//         ],
+//     [
+//         'tanggal_pinjam' => '20-05-01',
+//         'tanggal_kembali' => '20-5-10',
+//         'nama_peminjam' => 'eli',
+//         'alamat' => 'mataram',
+//         'buku_id' => 2
+//     ],
+// [
+//     'tanggal_pinjam' => '21-07-01',
+//     'tanggal_kembali' => '21-08-05',
+//     'nama_peminjam' => 'fina',
+//     'alamat' => 'waduwani',
+//     'buku_id' => 1
+// ]]);
     }
 }
+
